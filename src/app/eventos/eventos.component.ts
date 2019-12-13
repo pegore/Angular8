7fa419d6061d8f6b744eea645dd279265e651621
Lino Pegoretti <lino.hbsis@gmail.com>
2019-12-13 15:49:53 -0300

    adcionado bootstrap

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -18,7 +18,6 @@ export class EventosComponent implements OnInit {
       .get('https://de708a04-8e9e-4879-be24-2d7aac76be85.mock.pstmn.io/Eventos')
       .subscribe(
         response => {
-          console.log('Teste');
           this.eventos = response;
         },
         error => {
